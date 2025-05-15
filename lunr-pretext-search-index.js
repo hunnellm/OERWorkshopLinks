03ebac15ad8dafc994c19f51847882688a72b2be
Mark Hunnell <137966235+hunnellm@users.noreply.github.com>
2025-05-15 18:01:17 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -8,6 +8,15 @@ var ptx_lunr_docs = [
   "number": "",
   "title": "Links from OER Workshop 2025",
   "body": " Links from OER Workshop 2025    OER Workshop Participants      Collection of useful links from the AIM Workshop Open educational resources: adoption, curation, and customization    PreTeXt Resources   PreTeXt documentation for web hosting    PreTeXt for Instructors    GitHub for Mathematicians    Annotated example    PreTeXt codespace (GitHub)    PreTeXt Examples from documentation    Interactive Exercises    Publication file reference     Textbook Links  Discrete Open Math (textbook)   Understanding Linear Algebra (textbook)    Matrix Theory (textbook) Github    Linear Algebra for Team Based Inquiry Learning     PreFigure   Prefigure documentation    PreFigure Playground    PreFigure Documentation    PreFigure codespace (github)     Doenet   doenet.org    Doenet (alpha site)    Doenet Examples    Embed Doenet     Problem builders   WebWork Open Problem Library    GeoGebra    Desmos    MyOpenMath    Asymptote    Calcplot3d (applet)    CalcPlot3D (Documentation)    Runestone Academy   Runestone Academy    Example Files   TBIL Examples    Syllabus worksheet demo (GitHub)    Syllabus Worksheet demo (Website)    Tien Chih Github    Miscellaneous   Sage Cell Server       "
+},
+{
+  "id": "links-1",
+  "level": "2",
+  "url": "OER-2025-links.html#links-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Open educational resources: adoption, curation, and customization "
 }
 ]
 
